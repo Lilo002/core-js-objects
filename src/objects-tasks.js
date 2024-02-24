c8d3b7789192a7dbdbd9cfce1da77e8fc17b2666
Lilo002 <lisabasarab@gmail.com>
2024-02-24 10:22:02 +0300

    feat: add sellTickets solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -182,8 +182,25 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let change = 0;
+
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] === 50) {
+      if (change < 25) {
+        return false;
+      }
+    }
+    if (queue[i] === 100) {
+      if (change < 75) {
+        return false;
+      }
+    }
+
+    change += 25;
+  }
+
+  return true;
 }
 
 /**
